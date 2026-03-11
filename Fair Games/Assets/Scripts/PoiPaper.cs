6d65804b60f1fd97989c35c11752fbec59260fa8
SISI <schwermc@my.erau.edu>
2026-03-10 21:25:25 -0700

    Start on poi paper script
    Create poi paper script full of functions

---
+++ Fair Games/Assets/Scripts/PoiPaper.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PoiPaper : MonoBehaviour
+{
+    [SerializeField] bool broken = false;
+    [SerializeField] int usesLeft;
+    [SerializeField] float stregth;
+
+    private int randomNumber;
+    void Awake()
+    {
+        randomNumber = Random.Range(4, 34);
+    }
+
+    public PoiPaper()
+    {
+        usesLeft = randomNumber;
+        stregth = 100f;
+    }
+
+    public PoiPaper(int usesLeft = 17, float stregth = 100f)
+    {
+        this.usesLeft = usesLeft;
+        this.stregth = stregth;
+    }
+
+    public void UsePoi(int useAmount, float stregthAmount = 0)
+    {
+        usesLeft -= useAmount;
+        stregth -= stregthAmount;
+        CheckUsesLeft();
+    }
+
+    #region Poi In Water
+    public void WaterLogPoi(float timeInWater)
+    {
+        stregth -= timeInWater;
+        CheckUsesLeft();
+    }
+
+    public void WaterLogPoi(float stregthAmount, int useAmount = 0)
+    {
+        usesLeft -= useAmount;
+        stregth -= stregthAmount;
+        CheckUsesLeft();
+    }
+    #endregion
+
+    public void CheckUsesLeft()
+    {
+        if (usesLeft <= 0 || stregth <= 0)
+        {
+            broken = true;
+            return;
+        }
+    }
+
+    public bool GetBroken() { return broken; }
+    public (int, float) GetStatus() { return (usesLeft, stregth); }
+
+    public int NewRandomNumber() { return Random.Range(4, 34); }
+}
