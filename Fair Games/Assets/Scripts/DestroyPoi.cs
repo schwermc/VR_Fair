2cba9510b2889bb237326316b5802ddd7b2c4cf9
SISI <schwermc@my.erau.edu>
2026-04-02 17:26:36 -0700

    Decrease Ticket Amount
    When grabbing a poi, the amount of tickers the player has decreases.

--- Fair Games/Assets/Scripts/DestroyPoi.cs
+++ Fair Games/Assets/Scripts/DestroyPoi.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class DestroyPoi : MonoBehaviour
 {
+    [SerializeField] Currency currency;
+    [SerializeField] int increaseTickets = 5;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.transform.root.CompareTag("Poi"))
         {
+            currency.addToCurrency(increaseTickets);
             Destroy(other.gameObject.transform.root.gameObject);
         }
     }
--- Fair Games/Assets/Scripts/PoiBasket.cs
+++ Fair Games/Assets/Scripts/PoiBasket.cs
@@ -6,17 +6,24 @@ public class PoiBasket : MonoBehaviour
     [SerializeField] XRInteractionManager interactionManager;
     [SerializeField] XRBaseInteractor interactor;
 
-    [Header ("Poi")]
+    [Header("Poi")]
     [SerializeField] GameObject poiPrefab;
+
+    [Header("Tickets")]
+    [SerializeField] Currency currency;
+    [SerializeField] int decreaseAmount = 10;
 
     public void GrabSpawnClone()
     {
-        GameObject clone = Instantiate(poiPrefab, interactor.transform.position, Quaternion.identity);
-        XRBaseInteractable grabInteractable = clone.GetComponent<XRBaseInteractable>();
-
-        if (grabInteractable != null && interactionManager != null && interactor != null)
+        if (currency.getCurrentCurrencyAmount() >= decreaseAmount)
         {
-            interactionManager.SelectEnter((IXRSelectInteractor)interactor, (IXRSelectInteractable)grabInteractable);
+            GameObject clone = Instantiate(poiPrefab, interactor.transform.position, Quaternion.identity);
+            XRBaseInteractable grabInteractable = clone.GetComponent<XRBaseInteractable>();
+            if (grabInteractable != null && interactionManager != null && interactor != null)
+            {
+                currency.removeFromCurrency(decreaseAmount);
+                interactionManager.SelectEnter((IXRSelectInteractor)interactor, (IXRSelectInteractable)grabInteractable);
+            }
         }
     }
 }
